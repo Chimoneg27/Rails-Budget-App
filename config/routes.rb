20de921a37634a0b6cbeffff64f2fe0c8554a18a
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-01-11 17:48:53 +0200

    Update config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'splash', to: 'splash#index', as: 'splash'
+
+  devise_for :users
+
   resources :users
   resources :groups, only: [:index, :new, :create] do
     resources :purchases, only: [:index, :new, :create]
   end
 
-  root to: "groups#index"
+  authenticated :user do
+    root to: 'groups#index', as: :authenticated_root
+  end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # get "up" => "rails/health#show", as: :rails_health_check
+  unauthenticated :user do
+    root to: 'splash#index', as: :unauthenticated_root
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # root to: "groups#index"
 end
