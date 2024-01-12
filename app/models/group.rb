80a7456bae8c0236d483049c50c34a4bb8b7ab8e
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-01-12 03:34:45 +0200

    Add validations for models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,9 @@
 class Group < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :icon, presence: true
+  validates :amount, presence: true, numericality: true
   has_many :group_details, dependent: :destroy
   has_many :purchases, through: :group_details
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # validates name
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   has_many :groups
   has_many :purchases, foreign_key: 'author_id'
 end
